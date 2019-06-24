f5daf128b6391fd88f8c3424d58a567bb359ebfe
riag <riag@163.com>
2019-06-24 11:41:26 +0800

    winsudo 改为必选参数，不再使用默认 winsudo 路径

--- pywinutiltools/pywinservice.py
+++ pywinutiltools/pywinservice.py
@@ -1,14 +1,5 @@
 from pywinutiltools import powershell
 import click
-import platform
-import pkg_resources
-
-machine = platform.machine()
-if machine == 'amd64':
-    machine = 'x86_64'
-
-DEFAULT_SUDO_BIN = pkg_resources.resource_filename(__name__, 'bin/%s/sudo.exe' % machine)
-
 
 def list_service(winsudo_bin, name_pattern):
     cmd_list = ['get-service']
@@ -21,12 +12,9 @@ def list_service(winsudo_bin, name_pattern):
 
 def stop_service(winsudo_bin, name):
     powershell_bin = powershell.get_powershell_bin()
-    sudo_bin = winsudo_bin
-    if not winsudo_bin:
-        sudo_bin = DEFAULT_SUDO_BIN
 
     cmd_list = [
-        sudo_bin,
+        winsudo_bin,
         powershell_bin,
         '-NoProfile',
         '-Command',
@@ -40,11 +28,8 @@ def stop_service(winsudo_bin, name):
 def start_service(winsudo_bin, name):
 
     powershell_bin = powershell.get_powershell_bin()
-    sudo_bin = winsudo_bin
-    if not winsudo_bin:
-        sudo_bin = DEFAULT_SUDO_BIN
     cmd_list = [
-        sudo_bin,
+        winsudo_bin,
         powershell_bin,
         '-NoProfile',
         '-Command',
@@ -57,7 +42,7 @@ def start_service(winsudo_bin, name):
 
 @click.group()
 @click.pass_context
-@click.option('--winsudo-bin', default='', required=True)
+@click.option('--winsudo-bin', required=True)
 def cli(ctx, winsudo_bin):
     ctx.ensure_object(dict)
     ctx.obj['winsudo_bin'] = winsudo_bin
