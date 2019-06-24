b089b47b02b29af5c5daee2ce040d50d8beb76c2
riag <riag@163.com>
2019-06-24 16:54:55 +0800

    pyps 改为使用 Get-CimInstance 来获取进程信息

--- pywinutiltools/pyps.py
+++ pywinutiltools/pyps.py
@@ -5,14 +5,22 @@ import click
 @click.command()
 @click.argument('id_or_name', nargs=1, required=False)
 def cli(id_or_name):
-    cmd_list = ['Get-Process']
+    # Get-CimInstance  -Query "select * from Win32_Process where name like '%java%' " |Format-list ProcessId , CommandLine
+    # Get-CimInstance  -Query "select * from Win32_Process where ProcessId = 16632" |Format-list *
+    cmd_list = ['Get-CimInstance', '-Query']
+    sql = 'select * from Win32_Process'
+    where = ''
     if id_or_name:
         if str.isdigit(id_or_name):
-            cmd_list.append('-Id')
-        cmd_list.append(id_or_name)
+            where = 'ProcessId = %s' % id_or_name
+        else:
+            where = "name like '%%s%'" % id_or_name
+
+    if where:
+        sql = '%s where %s' % (sql, where)
 
     cmd_list.append('|')
-    cmd_list.append('Format-Table -Property Id, ProcessName, StartTime, Parent, Path')
+    cmd_list.append('Format-Table -Property ProcessId, ProcessName, Path, CommandLine')
     powershell.exec_command(' '.join(cmd_list))
 
 if __name__ == '__main__':
